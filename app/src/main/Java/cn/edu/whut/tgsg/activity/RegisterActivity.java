35115be2119e468c2b543e6ebff605ba7f7016c9
SiXiWanZii <yanglijin001@163.com>
2015-12-10 11:49:42 +0800

    测试

--- app/src/main/Java/cn/edu/whut/tgsg/activity/RegisterActivity.java
+++ app/src/main/Java/cn/edu/whut/tgsg/activity/RegisterActivity.java
@@ -1,7 +1,30 @@
 package cn.edu.whut.tgsg.activity;
 
+import android.content.Context;
+
+import cn.edu.whut.tgsg.base.BaseActivity;
+
 /**
  * Created by Administrator on 2015-12-10.
  */
-public class RegisterActivity {
+public class RegisterActivity extends BaseActivity{
+    @Override
+    protected int getContentLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected Context getContext() {
+        return null;
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
 }
